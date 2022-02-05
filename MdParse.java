2da0dba7b8d36b347f412e364eef00e873799edd
Brigette Hacia <94882496+bhacia@users.noreply.github.com>
2022-02-04 16:00:12 -0800

    fixed bug

--- MdParse.java
+++ MdParse.java
@@ -10,22 +10,40 @@ public class MdParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            if (markdown.indexOf(")", currentIndex) == -1) {
+            if (markdown.indexOf("(", currentIndex) == -1 || markdown.indexOf(")", currentIndex) == -1 ||
+                markdown.indexOf("[", currentIndex) == -1 || markdown.indexOf("]", currentIndex) == -1) {
                 break;
             }
+
+            if(markdown.indexOf("!", currentIndex) == currentIndex) {
+                break; //shouldn't be an image
+            }
+
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+
             int openParen = nextCloseBracket + 1;
             char possibleOpenParam = markdown.charAt(openParen);
-            if (nextOpenBracket == -1 || nextCloseBracket == -1) break;
-            if (possibleOpenParam != '(') break;
-            if (markdown.charAt(nextCloseBracket + 1) == '(' && (markdown.indexOf(")", currentIndex) < markdown.indexOf("[", nextCloseBracket) ||
-             markdown.indexOf("[", nextCloseBracket) == -1 && markdown.indexOf(")", currentIndex) != -1)) {
+            if (possibleOpenParam != '(') {
+                break; //check if openParen is actually "(" for valid link
+            }
+
+            int openParenInSite = markdown.indexOf("(", openParen + 1);
+            if(openParenInSite != -1 && openParenInSite < markdown.indexOf("[", openParen)) {
+                int closeParenInSite = markdown.indexOf(")", openParenInSite);
+                if(openParenInSite != -1 && closeParenInSite != -1) {
+                    int closeParen = markdown.indexOf(")", closeParenInSite + 1);
+                    if(closeParen < markdown.indexOf("[", nextCloseBracket)) {
+                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                        currentIndex = closeParen + 1;
+                    }
+                }
+            }
+            else {
+                //else: there's no extra open paren
                 int closeParen = markdown.indexOf(")", openParen);
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
-            } else {
-                currentIndex++;
             }
         }
         return toReturn;
